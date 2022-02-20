6c28068fb9e805ae3039b447018e57d3a1600209
Sergei <kvakazuabr@yandex.ru>
2022-02-20 21:29:40 +0300

    added routing in project

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,34 @@
 import Main from '../main/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SingIn from '../sing_in/SingIn';
+import Favorites from '../favorites/Favorites';
+import Property from '../property/Property';
+import NotFound from '../not_found/NotFound';
+import PrivateRoute from '../private_route/PrivateRoute';
 
 type AppProps = {
   cityList: Array<string>;
 };
 
-function App({cityList}: AppProps): JSX.Element {
-  return <Main cityList={cityList} />;
+function App({ cityList }: AppProps): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main cityList={cityList} />} />
+        <Route path="/login" element={<SingIn />} />
+        <Route path="/favorites" element={
+          <PrivateRoute>
+            <Favorites />
+          </PrivateRoute>
+        }
+        />
+        <Route path="/offer/:id" element={<Property />} >
+          <Route path=":id" element={<Property />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
